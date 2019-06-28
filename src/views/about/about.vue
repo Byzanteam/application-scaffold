530145ae8a596f6c69840b5763cb6e7998ef5099
vanppo <vanppo@hotmail.com>
2019-06-28 11:06:31 +0800

    TEMP

--- src/views/about/about.vue
+++ src/views/about/about.vue
@@ -1,3 +1,17 @@
 <template>
-  <center>ABOUT</center>
+  <div>
+    <Avatar/>
+  </div>
 </template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+import {Avatar} from '~na~me~'
+
+@Component({
+  components: {
+    Avatar,
+  },
+})
+export default class About extends Vue {}
+</script>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -8,7 +8,8 @@
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator'
-import Logo from 'components/logo'
+import {Logo} from '~na~me~'
+// import Logo from 'components/logo'
 
 @Component({
   components: {
